f967210d39e124353aa1f606302f22f7652d7233
TT <you@example.com>
2025-10-26 14:37:46 +0100

    updates

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 
 const Loader = () => {
   const [fadeOut, setFadeOut] = useState(false);
